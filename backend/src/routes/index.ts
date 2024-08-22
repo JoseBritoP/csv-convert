3c82d0b0deccbff1c7e482c995991b2258cdd148
José Brito <josealbritose@gmail.com>
2024-08-22 20:02:17 -0300

    API init

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,21 @@
+import { Request,Response,NextFunction,ErrorRequestHandler, Router } from "express";
+
+const mainRouter = Router();
+
+mainRouter.get('/',(req,res)=>{
+  return res.send('hello')
+})
+
+mainRouter.use((req:Request, res:Response, next:NextFunction) => {
+  const error:any = new Error(`La ruta ${req.originalUrl} con el método ${req.method} no está implementada`);
+  error.status = 404;
+  next(error);
+});
+
+mainRouter.use((error:any, req:Request, res:Response, next:NextFunction) => {
+  res.status(error.status || 500).json({
+    message: error.message || 'Error interno del servidor'
+  });
+});
+
+export default mainRouter
