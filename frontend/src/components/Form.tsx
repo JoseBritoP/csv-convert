e5358876aa594fa57383ca5126b926035ee25703
José Brito <josealbritose@gmail.com>
2024-09-06 01:09:48 -0300

    Style Form

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -7,21 +7,32 @@ export default function Form() {
   };
   return (
     <form className="border-gray-100 p-4 flex flex-col">
-      <div className="flex items-center gap-6 border-2 p-4 rounded-lg">
-        <label htmlFor="file" className="text-center text-slate-200 font-semibold">CSV File</label>
-        <input
-          onChange={handleInputChange}
-          type="file"
-          name="file"
-          id="file"
-          accept=".csv"
-          className="text-gray-200 px-4 font-semibold "
-        />
-      </div>
-      <div className="flex items-center w-full justify-evenly pt-4">
-        <button className="text-emerald-500 border-2 border-emerald-600 px-2 py-4 rounded-lg font-semibold hover:bg-emerald-950 transition-colors duration-300 ease-in-out hover:text-emerald-300">Upload file</button>
-        <button className="text-red-500 border-2 border-red-600 px-2 py-4 rounded-lg font-semibold hover:bg-red-950 hover:text-red-300 transition-colors duration-300 ease-in-out">Remove file</button>
-      </div>
+      <fieldset className="border-2 p-6 border-gray-500 rounded-lg">
+        <div className="flex items-center gap-6 border-2 p-4 rounded-lg">
+          <label
+            htmlFor="file"
+            className="text-center text-slate-200 font-semibold cursor-pointer"
+          >
+            CSV File
+          </label>
+          <input
+            onChange={handleInputChange}
+            type="file"
+            name="file"
+            id="file"
+            accept=".csv"
+            className="text-gray-200 px-4 font-semibold "
+          />
+        </div>
+        <div className="flex items-center w-full justify-evenly pt-4">
+          <button className="text-emerald-500 border-2 border-emerald-600 px-2 py-4 rounded-lg font-semibold hover:bg-emerald-950 transition-colors duration-300 ease-in-out hover:text-emerald-300">
+            Upload file
+          </button>
+          <button className="text-red-500 border-2 border-red-600 px-2 py-4 rounded-lg font-semibold hover:bg-red-950 hover:text-red-300 transition-colors duration-300 ease-in-out">
+            Remove file
+          </button>
+        </div>
+      </fieldset>
     </form>
   );
 }
